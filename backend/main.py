39e9c6efe97b864dfcf27915f1a5a9c5529a41d3
Parin Parikh <parinparikh1@gmail.com>
2024-02-20 15:13:14 -0500

    api requests

---
+++ backend/main.py
@@ -0,0 +1,52 @@
+from flask import request, jsonify
+from config import app, db
+from models import Contact
+
+@app.route("/contacts", methods=["GET"])
+def get_contacts():
+    contacts = Contact.query.all()
+    json_contacts = list(map(lambda x: x.to_json(), contacts))
+    return jsonify({"contacts": json_contacts})
+
+@app.route("/create_contact", methods=["POST"])
+def create_contact():
+    first_name = request.json.get("firstName")
+    last_name = request.json.get("lastName")
+    email = request.json.get("email")
+
+    if not first_name or not last_name or not email:
+        return (
+            jsonify({"message": "You must include a first name, last name and email"}), 
+            400,
+        )
+
+    new_contact = Contact(first_name=first_name, last_name=last_name, email=email)
+    try:
+        db.session.add(new_contact)
+        db.session.commit()
+    except Exception as e:
+        return jsonify({"message": str(e)}), 400
+    
+    return jsonify({"message": "User Created!"}), 201
+    
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+
+    app.run(debug=True)
+
+
+
+
+# Create
+# localhost:5000/create_contact
+
+
+# Read
+# localhost:5000/read_contact
+
+# Update 
+# localhost:5000/update_contact
+
+# Delete
+# localhost:5000/delete_contact
